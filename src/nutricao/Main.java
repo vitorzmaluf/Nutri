d67696bf337f96121ad61e52a88279efa6321ec0
Danilo Bizarria <danilobizarria@gmail.com>
2019-03-26 09:30:20 -0300

    Add do-while main command-line menu

--- src/nutricao/Main.java
+++ src/nutricao/Main.java
@@ -1,22 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package nutricao;
 
+package nutricao;
+import java.util.Scanner;
 /**
  *
  * @author unifdaoliveira
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Usuario u = new Usuario();
+    public static void main(String[] args){
+        Usuario usuario = null;
+        Scanner input = null;
+        
+        
+        int opc = 0;
+        do{
+            System.out.println("-------------------------------------------");
+            System.out.println("Digite 1 para criar seu perfl");
+            System.out.println("Digite 2 para visualizar seus dados");
+            System.out.println("Digite 3 para alterar seus dados");
+            System.out.println("Digite 4 para deletar seu perfil");
+            System.out.println("Digite 5 para encerrar o programa");
+            System.out.println("-------------------------------------------");
+            
+            input = new Scanner(System.in);
+            if (input.hasNext()) opc = input.nextInt();
+            else opc = 5;
+            input.close();
+            
+            if (opc == 1) /*Usuario.cadastrar()*/;
+            else if (usuario == null || opc != 5) System.out.println("Seu perfil ainda não foi criado!");
+            else{
+                if (opc == 2) /*usuario.visualizar()*/;
+                if (opc == 3) /*usuario.alterar()*/;
+                if (opc == 4) /*usuario.deletar()*/;
+            }
+            
+        }while(opc != 5);
+        
+        
         
 //        u.dieta = Calculo.fazDieta();
     }
