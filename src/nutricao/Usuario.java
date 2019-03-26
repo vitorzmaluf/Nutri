aa983a0c92faf7c2163a81f6d8b915c3f49178b0
Danilo Bizarria <danilobizarria@gmail.com>
2019-03-26 09:33:03 -0300

    Add Usuario's gets, sets and a constructor

--- src/nutricao/Usuario.java
+++ src/nutricao/Usuario.java
@@ -1,23 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package nutricao;
+import java.util.Scanner;
 
-
-/**
- *
- * @author unifdaoliveira
- */
 public class Usuario {    
-    public enum Biotipo {
+    public static enum Biotipo {
         Ectomorfo,
         Mesomorfo,
         Endomorfo
     }
     
-    public enum Objetivo {
+    public static enum Objetivo {
         Emagrecimento,
         Hipertrofia,
         Definicao
@@ -26,9 +18,9 @@ public class Usuario {
     private String nome;
     private int idade;
     private double peso;
-    private int altura;
+    private double altura;
     private Biotipo biotipo;
-    private double imc;
+//    private double imc;
     
     // implementar classes
 //    private FichaMedica fichaMedica;
@@ -38,9 +30,91 @@ public class Usuario {
     
     // plano nutricional
     private Dieta dietaSugerida;
-    private Treino treinoSugerida;
+    private Treino treinoSugerido;
+    
+    public double getAltura() {
+        return altura;
+    }
     
     public double getPeso() {
-        return this.peso;
+        return peso;
     }
+    
+    public double getImc() {
+        return getPeso()/Math.pow(getAltura(), 2);
+//        (getAltura()*getAltura());
+    }
+    
+    protected void setObjetivo(Objetivo objetivo) {
+        this.objetivo = objetivo; 
+    }
+    
+    public void setObjetivo(char digitado) {
+        if (digitado == 'e' || digitado == 'E') {
+            setObjetivo(Objetivo.Emagrecimento);
+        }
+        if (digitado == 'h' || digitado == 'H') {
+            setObjetivo(Objetivo.Hipertrofia);
+        }
+        if (digitado == 'd' || digitado == 'D') {
+            setObjetivo(Objetivo.Definicao);
+        }
+    }
+    
+    protected void setBiotipo(Biotipo bio){
+        biotipo = bio;
+    }
+    
+    public void setBiotipo(String digitado){
+        String upper = digitado.toUpperCase();
+        if (upper.equals("EC")) setBiotipo(Biotipo.Ectomorfo);
+        if (upper.equals("EN")) setBiotipo(Biotipo.Endomorfo);
+        if (upper.equals("ME")) setBiotipo(Biotipo.Mesomorfo);
+    }
+    
+    public Usuario(){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Digite seu nome");
+        String nome = input.skip("\n").nextLine();
+        setNome(nome);
+    
+        System.out.println("Digite sua idade");
+        int idade = input.nextInt();
+        setIdade(idade);
+        
+        System.out.println("Digite seu peso");
+        double peso = input.nextDouble();
+        setPeso(peso);
+        
+        System.out.println("Digite seu altura");
+        double altura = input.nextDouble();
+        setAltura(altura);
+                
+        System.out.println("Digite seu Objetivo (H: Hipertrofia, E: Emagrecimento ou D: Definicao)");
+        char objetivo = input.next().charAt(0);        
+        setObjetivo(objetivo);
+        
+        System.out.println("Digite seu Biotipo (Ec: Ectomorfo, En: Endmorfo ou Me: Mesomorfo)");
+        String biotipo = input.next();        
+        setBiotipo(biotipo);
+    }
+    
+   public void setNome(String nome){
+       this.nome = nome;
+   }
+    
+   public void setIdade(int id){
+       this.idade = id;
+   }
+
+   public void setPeso(double peso){
+       this.peso = peso;
+   }
+   
+   public void setAltura(double altura){
+       this.altura = altura;
+   }
+    
+    
 }
