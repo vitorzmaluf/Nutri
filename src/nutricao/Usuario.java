f389c0ad2955e7d8a7c9969a281d744f424b4795
Danilo Bizarria <danilobizarria@gmail.com>
2019-03-26 09:33:54 -0300

    VS Code refactorings

--- src/nutricao/Usuario.java
+++ src/nutricao/Usuario.java
@@ -1,54 +1,83 @@
 
 package nutricao;
+
 import java.util.Scanner;
 
-public class Usuario {    
+public class Usuario {
     public static enum Biotipo {
-        Ectomorfo,
-        Mesomorfo,
+        Ectomorfo, 
+        Mesomorfo, 
         Endomorfo
     }
-    
+
     public static enum Objetivo {
-        Emagrecimento,
-        Hipertrofia,
+        Emagrecimento, 
+        Hipertrofia, 
         Definicao
     }
-    
+
     private String nome;
     private int idade;
     private double peso;
     private double altura;
     private Biotipo biotipo;
-//    private double imc;
-    
+    // private double imc;
+
     // implementar classes
-//    private FichaMedica fichaMedica;
+    // private FichaMedica fichaMedica;
     private Objetivo objetivo;
     private Dieta dietaAtual;
     private Treino treinoAtual;
-    
+
     // plano nutricional
     private Dieta dietaSugerida;
     private Treino treinoSugerido;
-    
+
+    public Usuario() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite seu nome");
+        String nome = input.skip("\n").nextLine();
+        setNome(nome);
+
+        System.out.println("Digite sua idade");
+        int idade = input.nextInt();
+        setIdade(idade);
+
+        System.out.println("Digite seu peso");
+        double peso = input.nextDouble();
+        setPeso(peso);
+
+        System.out.println("Digite seu altura");
+        double altura = input.nextDouble();
+        setAltura(altura);
+
+        System.out.println("Digite seu Objetivo (H: Hipertrofia, E: Emagrecimento ou D: Definicao)");
+        char objetivo = input.next().charAt(0);
+        setObjetivo(objetivo);
+
+        System.out.println("Digite seu Biotipo (Ec: Ectomorfo, En: Endmorfo ou Me: Mesomorfo)");
+        String biotipo = input.next();
+        setBiotipo(biotipo);
+    }
+
     public double getAltura() {
         return altura;
     }
-    
+
     public double getPeso() {
         return peso;
     }
-    
+
     public double getImc() {
-        return getPeso()/Math.pow(getAltura(), 2);
-//        (getAltura()*getAltura());
+        return getPeso() / Math.pow(getAltura(), 2);
+        // (getAltura()*getAltura());
     }
-    
+
     protected void setObjetivo(Objetivo objetivo) {
-        this.objetivo = objetivo; 
+        this.objetivo = objetivo;
     }
-    
+
     public void setObjetivo(char digitado) {
         if (digitado == 'e' || digitado == 'E') {
             setObjetivo(Objetivo.Emagrecimento);
@@ -60,61 +89,35 @@ public class Usuario {
             setObjetivo(Objetivo.Definicao);
         }
     }
-    
-    protected void setBiotipo(Biotipo bio){
+
+    protected void setBiotipo(Biotipo bio) {
         biotipo = bio;
     }
-    
-    public void setBiotipo(String digitado){
-        String upper = digitado.toUpperCase();
-        if (upper.equals("EC")) setBiotipo(Biotipo.Ectomorfo);
-        if (upper.equals("EN")) setBiotipo(Biotipo.Endomorfo);
-        if (upper.equals("ME")) setBiotipo(Biotipo.Mesomorfo);
-    }
-    
-    public Usuario(){
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("Digite seu nome");
-        String nome = input.skip("\n").nextLine();
-        setNome(nome);
-    
-        System.out.println("Digite sua idade");
-        int idade = input.nextInt();
-        setIdade(idade);
-        
-        System.out.println("Digite seu peso");
-        double peso = input.nextDouble();
-        setPeso(peso);
-        
-        System.out.println("Digite seu altura");
-        double altura = input.nextDouble();
-        setAltura(altura);
-                
-        System.out.println("Digite seu Objetivo (H: Hipertrofia, E: Emagrecimento ou D: Definicao)");
-        char objetivo = input.next().charAt(0);        
-        setObjetivo(objetivo);
-        
-        System.out.println("Digite seu Biotipo (Ec: Ectomorfo, En: Endmorfo ou Me: Mesomorfo)");
-        String biotipo = input.next();        
-        setBiotipo(biotipo);
-    }
-    
-   public void setNome(String nome){
-       this.nome = nome;
-   }
-    
-   public void setIdade(int id){
-       this.idade = id;
-   }
 
-   public void setPeso(double peso){
-       this.peso = peso;
-   }
-   
-   public void setAltura(double altura){
-       this.altura = altura;
-   }
-    
-    
+    public void setBiotipo(String digitado) {
+        String upper = digitado.toUpperCase();
+        if (upper.equals("EC"))
+            setBiotipo(Biotipo.Ectomorfo);
+        if (upper.equals("EN"))
+            setBiotipo(Biotipo.Endomorfo);
+        if (upper.equals("ME"))
+            setBiotipo(Biotipo.Mesomorfo);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int id) {
+        this.idade = id;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
 }
