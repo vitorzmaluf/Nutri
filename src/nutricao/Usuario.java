84066ee01179e8b6fdf0b3e9302f1bcad6bf771f
DANILO BIZARRIA DE OLIVEIRA <unifdaoliveira>
2019-03-29 19:35:16 -0300

    initial lab commit

--- src/nutricao/Usuario.java
+++ src/nutricao/Usuario.java
@@ -1,5 +1,6 @@
 
 package nutricao;
+<<<<<<< HEAD
 
 import java.util.Scanner;
 
@@ -16,11 +17,29 @@ public class Usuario {
         Definicao
     }
 
+=======
+import java.util.Scanner;
+
+public class Usuario {    
+    public static enum Biotipo {
+        Ectomorfo,
+        Mesomorfo,
+        Endomorfo
+    }
+    
+    public static enum Objetivo {
+        Emagrecimento,
+        Hipertrofia,
+        Definicao
+    }
+    
+>>>>>>> initial lab commit
     private String nome;
     private int idade;
     private double peso;
     private double altura;
     private Biotipo biotipo;
+<<<<<<< HEAD
     // private double imc;
 
     // implementar classes
@@ -78,6 +97,37 @@ public class Usuario {
         this.objetivo = objetivo;
     }
 
+=======
+//    private double imc;
+    
+    // implementar classes
+//    private FichaMedica fichaMedica;
+    private Objetivo objetivo;
+    private Dieta dietaAtual;
+    private Treino treinoAtual;
+    
+    // plano nutricional
+    private Dieta dietaSugerida;
+    private Treino treinoSugerido;
+    
+    public double getAltura() {
+        return altura;
+    }
+    
+    public double getPeso() {
+        return peso;
+    }
+    
+    public double getImc() {
+        return getPeso()/Math.pow(getAltura(), 2);
+//        (getAltura()*getAltura());
+    }
+    
+    protected void setObjetivo(Objetivo objetivo) {
+        this.objetivo = objetivo; 
+    }
+    
+>>>>>>> initial lab commit
     public void setObjetivo(char digitado) {
         if (digitado == 'e' || digitado == 'E') {
             setObjetivo(Objetivo.Emagrecimento);
@@ -89,6 +139,7 @@ public class Usuario {
             setObjetivo(Objetivo.Definicao);
         }
     }
+<<<<<<< HEAD
 
     protected void setBiotipo(Biotipo bio) {
         biotipo = bio;
@@ -120,4 +171,66 @@ public class Usuario {
         this.altura = altura;
     }
 
+=======
+    
+    protected void setBiotipo(Biotipo bio){
+        biotipo = bio;
+    }
+    
+    public void setBiotipo(String digitado){
+        String upper = digitado.toUpperCase();
+        if (upper.equals("EC")) setBiotipo(Biotipo.Ectomorfo);
+        if (upper.equals("EN")) setBiotipo(Biotipo.Endomorfo);
+        if (upper.equals("ME")) setBiotipo(Biotipo.Mesomorfo);
+    }
+    
+    public static Usuario cadastrar(){
+        Scanner input = new Scanner(System.in);
+        Usuario usuario = new Usuario();
+        
+        System.out.println("Digite seu nome");
+        String nome = input.skip("\n").nextLine();
+        usuario.setNome(nome);
+    
+        System.out.println("Digite sua idade");
+        int idade = input.nextInt();
+        usuario.setIdade(idade);
+        
+        System.out.println("Digite seu peso");
+        double peso = input.nextDouble();
+        usuario.setPeso(peso);
+        
+        System.out.println("Digite seu altura");
+        double altura = input.nextDouble();
+        usuario.setAltura(altura);
+                
+        System.out.println("Digite seu Objetivo (H: Hipertrofia, E: Emagrecimento ou D: Definicao)");
+        char objetivo = input.next().charAt(0);        
+        usuario.setObjetivo(objetivo);
+        
+        System.out.println("Digite seu Biotipo (Ec: Ectomorfo, En: Endmorfo ou Me: Mesomorfo)");
+        String biotipo = input.next();        
+        usuario.setBiotipo(biotipo);
+        
+        return usuario;
+    }
+    
+   public void setNome(String nome){
+       this.nome = nome;
+   }
+    
+   public void setIdade(int id){
+       this.idade = id;
+   }
+
+   public void setPeso(double peso){
+       this.peso = peso;
+   }
+   
+   public void setAltura(double altura){
+       this.altura = altura;
+   }
+    
+    
+>>>>>>> initial lab commit
 }
